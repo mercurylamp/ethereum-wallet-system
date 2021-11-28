82e1d011bc60ec01b67fce1acf15f6ad72e235f2
mercurylamp <pakgunwoo@gmail.com>
2021-11-28 16:14:26 +0900

    spring boot init

---
+++ src/main/java/com/example/ethereumwalletsystem/EthereumWalletSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.ethereumwalletsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthereumWalletSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EthereumWalletSystemApplication.class, args);
+	}
+
+}
